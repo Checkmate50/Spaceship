ec9dde3d05f965188b154f4fac09906e4c9b9fee
Checkmate50 <dgeisler50@gmail.com>
2016-08-22 17:45:35 -0600

    Basic networking stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private const float movespeed = 5f;
+
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * movespeed;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
+
+        transform.position += new Vector3(x, y);
+    }
+}
