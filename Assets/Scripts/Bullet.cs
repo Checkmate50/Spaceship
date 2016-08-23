a35d3497efc74925f47f77a8d329f294112646b4
Penumbra\Elliot <elliotm555@gmail.com>
2016-08-22 18:59:07 -0600

    Added a bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public int speed = 5;
+
+	void Update ()
+    {
+        transform.position += new Vector3(3, 0, 0) * Time.deltaTime;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using System.Collections;
 
 public class PlayerController : NetworkBehaviour {
 
-    private const float movespeed = 5f;
+    private const float movespeed = 3f;
+    public Bullet bullet;
 
-	// Update is called once per frame
-	void Update () {
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<SpriteRenderer>().color = Color.blue;
+    }
+
+    void Update () {
+
         if (!isLocalPlayer)
             return;
-        
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * movespeed;
-        var y = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Instantiate(bullet, transform.position, Quaternion.identity);
+
+        var x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * movespeed;
+        var y = Input.GetAxisRaw("Vertical") * Time.deltaTime * movespeed;
 
         transform.position += new Vector3(x, y);
     }
