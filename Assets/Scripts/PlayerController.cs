d083ec0b706aba28df9f5cccf39ca401d69a3373
Checkmate50 <dgeisler50@gmail.com>
2016-08-22 19:18:06 -0600

    Added syncing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class PlayerController : NetworkBehaviour {
 
     private const float movespeed = 3f;
-    public Bullet bullet;
+    public GameObject bullet;
 
     public override void OnStartLocalPlayer()
     {
@@ -18,14 +18,17 @@ public class PlayerController : NetworkBehaviour {
             return;
 
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject temp = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            NetworkServer.Spawn(temp);
-        }
+            CmdFire();
 
         var x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * movespeed;
         var y = Input.GetAxisRaw("Vertical") * Time.deltaTime * movespeed;
 
         transform.position += new Vector3(x, y);
+    }
+
+    [Command]
+    void CmdFire() {
+        GameObject temp = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+        NetworkServer.Spawn(temp);
     }
 }
